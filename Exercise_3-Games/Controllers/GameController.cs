19813204a0a4e0238eaed99d4106d94a5862daeb
Aaron Dayrit <aarondayrit@Aarons-MacBook-Air.local>
2022-11-30 22:52:53 -0600

    inital commit

---
+++ Exercise_3-Games/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Exercise_3_Games.Models;
+
+namespace Exercise_3_Games.Controllers;
+
+public class GameController : Controller
+{
+    private readonly ILogger<GameController> _logger;
+
+    public GameController(ILogger<GameController> logger)
+    {
+        _logger = logger;
+    }
+
+    //default view
+    public IActionResult all()
+    {
+        var gamesRepo = new VideoGameRepository();
+        return View(gamesRepo);
+    }
+
+    /* select through the video game repo.Games
+        * get the one with the id]
+        * return it to the view
+     */
+
+    public IActionResult game(string id)
+    {
+        HashSet<VideoGame> VideGameList = new VideoGameRepository().VideoGames;
+        IEnumerable<VideoGame> targetGame = VideGameList.Where(game => game.Title == id);
+        VideoGame result = targetGame.First();
+        return View(result);
+    }
+}
+
---
+++ Exercise_3-Games/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercise_3_Games.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
